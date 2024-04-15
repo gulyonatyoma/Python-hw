d3ac9729d6e2d390ec73b371d75b8db1ef5bcd44
Artyom Gostev <artyomgostev26082005@gmail.com>
2024-04-15 15:32:53 +0300

    fixed graphic

--- graphic.py
+++ graphic.py
@@ -6,12 +6,13 @@ X = np.linspace(-10, 10, 100)
 Y = np.linspace(-10, 10, 100)
 X, Y = np.meshgrid(X, Y)
 R = X ** 2 + Y ** 2
-Z = np.sin(R) / R
+Z = np.sin(R ** 0.5) / (R ** 0.5)
 
 # Plot the surface
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 surface = ax.plot_surface(X, Y, Z, vmin=Z.min() * 2, cmap=cm.viridis)
 
 fig.colorbar(surface)
+ax.set_zlim(-0.2, 0.8)
 
 plt.show()
