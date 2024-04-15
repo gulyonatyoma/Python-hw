099b7133cc7b7778abae7efe83ba9165c413315d
Artyom Gostev <artyomgostev26082005@gmail.com>
2024-04-15 15:39:39 +0300

    fixed graphic

--- graphic.py
+++ graphic.py
@@ -8,7 +8,6 @@ X, Y = np.meshgrid(X, Y)
 R = X ** 2 + Y ** 2
 Z = np.sin(R ** 0.5) / (R ** 0.5)
 
-# Plot the surface
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 surface = ax.plot_surface(X, Y, Z, vmin=Z.min() * 2, cmap=cm.viridis)
 
